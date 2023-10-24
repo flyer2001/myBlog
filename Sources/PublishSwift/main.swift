50dd530bfeb9a7f0ed70ba68eb43742003d614eb
Sergey Popyvanov <sgpopyvanov@avito.ru>
2023-10-25 01:45:14 +0400

    Модификатор для обработки вики-ссылок

--- Sources/PublishSwift/main.swift
+++ Sources/PublishSwift/main.swift
@@ -16,7 +16,7 @@ struct PublishSwift: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://s.popyvanov.ru/blog/")!
+    var url = URL(string: "https://s.popyvanov.ru/")!
     var name = "Цифровой садик Попыванова Сергея"
     var description = "заметки, которые не помещаются в блокнот"
     var language: Language { .russian }
@@ -36,9 +36,52 @@ extension PublishingStep {
     static func generateHtmlFromMarkdownFiles() -> Self {
         step(named: "Создаю html файлы в кастомном шаге") { context in
             let folder = try context.folder(at: "Content")
-            // TODO - место для кастомных модификаторов
+            
+            let subFolderResult = try context.folder(at: "Content/posts")
+            
+            var paths: [String: String] = [:]
+            try subFolderResult.files.forEach { file in
+                let markdown = try context.markdownParser.parse(file.readAsString())
+                let path = markdown.metadata["path"]
+                let name = String(file.name.dropLast(3)) // remove .md extension
+                paths[name] = path //
+            }
+            context.markdownParser.addModifier(.wikiLinker(paths: paths, postsPathUrl: context.site.url.absoluteString + "posts/"))
             
             try await MarkdownFileHandler().addMarkdownFiles(in: folder, to: &context)
+        }
+    }
+}
+
+public extension Modifier {
+    static func wikiLinker(paths: [String:String], postsPathUrl: String) -> Self {
+        Modifier(target: .paragraphs) { html, markdown in
+            var result = html
+            let regex = try! NSRegularExpression(pattern: "\\[\\[(.*?)\\]\\]", options: [])
+            
+            let matches = regex.matches(
+                in: html,
+                options: [],
+                range: NSRange(location: 0, length: html.utf16.count)
+            )
+            
+            for match in matches.reversed() {
+                let rangeOfLinkWithoutBrackets = match.range(at: 1)
+                var outputLink = "" 
+                if let range = Range(rangeOfLinkWithoutBrackets, in: html) {
+                    let wikiLinkString = String(html[range])
+                    // discover - статья заглушка
+                    let urlPath = postsPathUrl + (paths[wikiLinkString] ?? "discover")
+                    
+                    outputLink = "<a href=\"\(urlPath)\">\(wikiLinkString)</a>"
+                    
+                    let range = Range(match.range, in: result)!
+                    
+                    result = result.replacingCharacters(in: range, with: outputLink)
+                }
+            }
+
+            return result
         }
     }
 }
