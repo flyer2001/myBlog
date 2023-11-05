7c85dce93172c791511eda09cd586cb0ad1c99a4
Sergey Popyvanov <sgpopyvanov@avito.ru>
2023-11-05 22:27:35 +0400

    Добавление вики ссылок в список

--- Sources/PublishSwift/main.swift
+++ Sources/PublishSwift/main.swift
@@ -46,7 +46,8 @@ extension PublishingStep {
                 let name = String(file.name.dropLast(3)) // remove .md extension
                 paths[name] = path //
             }
-            context.markdownParser.addModifier(.wikiLinker(paths: paths, postsPathUrl: context.site.url.absoluteString + "posts/"))
+            context.markdownParser.addModifier(.paragraphWikiLinker(paths: paths, postsPathUrl: context.site.url.absoluteString + "posts/"))
+            context.markdownParser.addModifier(.listsWikiLinker(paths: paths, postsPathUrl: context.site.url.absoluteString + "posts/"))
             
             try await MarkdownFileHandler().addMarkdownFiles(in: folder, to: &context)
         }
@@ -54,34 +55,44 @@ extension PublishingStep {
 }
 
 public extension Modifier {
-    static func wikiLinker(paths: [String:String], postsPathUrl: String) -> Self {
-        Modifier(target: .paragraphs) { html, markdown in
-            var result = html
-            let regex = try! NSRegularExpression(pattern: "\\[\\[(.*?)\\]\\]", options: [])
-            
-            let matches = regex.matches(
-                in: html,
-                options: [],
-                range: NSRange(location: 0, length: html.utf16.count)
-            )
-            
-            for match in matches.reversed() {
-                let rangeOfLinkWithoutBrackets = match.range(at: 1)
-                var outputLink = "" 
-                if let range = Range(rangeOfLinkWithoutBrackets, in: html) {
-                    let wikiLinkString = String(html[range])
-                    // discover - статья заглушка
-                    let urlPath = postsPathUrl + (paths[wikiLinkString] ?? "discover")
-                    
-                    outputLink = "<a href=\"\(urlPath)\">\(wikiLinkString)</a>"
-                    
-                    let range = Range(match.range, in: result)!
-                    
-                    result = result.replacingCharacters(in: range, with: outputLink)
-                }
+    private static func transformToWikiLink(html: String, paths: [String: String], postsPathUrl: String) -> String {
+        var result = html
+        let regex = try! NSRegularExpression(pattern: "\\[\\[(.*?)\\]\\]", options: [])
+        
+        let matches = regex.matches(
+            in: html,
+            options: [],
+            range: NSRange(location: 0, length: html.utf16.count)
+        )
+        
+        for match in matches.reversed() {
+            let rangeOfLinkWithoutBrackets = match.range(at: 1)
+            var outputLink = ""
+            if let range = Range(rangeOfLinkWithoutBrackets, in: html) {
+                let wikiLinkString = String(html[range])
+                // discover - статья заглушка
+                let urlPath = postsPathUrl + (paths[wikiLinkString] ?? "discover")
+                
+                outputLink = "<a href=\"\(urlPath)\">\(wikiLinkString)</a>"
+                
+                let range = Range(match.range, in: result)!
+                
+                result = result.replacingCharacters(in: range, with: outputLink)
             }
+        }
 
-            return result
+        return result
+    }
+    
+    static func paragraphWikiLinker(paths: [String:String], postsPathUrl: String) -> Self {
+        Modifier(target: .paragraphs) { html, _ in
+            transformToWikiLink(html: html, paths: paths, postsPathUrl: postsPathUrl)
+        }
+    }
+    
+    static func listsWikiLinker(paths: [String:String], postsPathUrl: String) -> Self {
+        Modifier(target: .lists) { html, _ in
+            transformToWikiLink(html: html, paths: paths, postsPathUrl: postsPathUrl)
         }
     }
 }
